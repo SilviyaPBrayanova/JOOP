f278b38a1c4c633974fc9d5e6ec1d0ed1b02ee6d
kotelisk <silviya.brayanova@gmail.com>
2016-05-30 22:25:31 +0300

    Adding the Shape3d.java

---
+++ lab13/src/uni/sofia/fmi/master/tzi/Shapes3D.java
@@ -0,0 +1,15 @@
+package uni.sofia.fmi.master.tzi;
+
+public class Shapes3D {
+	 public void play(){
+		 
+	 }
+	 
+	 public static void dumpShapes3D(Shapes3D [] shapes){
+		 
+	 }
+	 
+	 public double volume(){
+		 return 0;
+	 }
+}
