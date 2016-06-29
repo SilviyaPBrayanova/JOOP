30a6cc389396e69417df261fdd75c1642aa317c6
kotelisk <silviya.brayanova@gmail.com>
2016-06-29 22:00:09 +0300

    final and lab14 changes

---
+++ final/src/uni/sofia/fmi/master/tzi/YAMLRedactor.java
@@ -0,0 +1,28 @@
+package uni.sofia.fmi.master.tzi;
+
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import uni.sofia.fmi.master.tzi.ui.RedactorPanel;
+
+public class YAMLRedactor {
+
+	public static void main(String[] args) throws InvocationTargetException, InterruptedException {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				RedactorPanel rp = new RedactorPanel();
+				JFrame application = new JFrame();
+
+				application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				application.add(rp);
+				application.setSize(500, 400);
+				application.setResizable(false);
+				application.setVisible(true);
+			}
+		});
+	}
+}
